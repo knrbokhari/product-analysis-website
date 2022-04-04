ef56c5f568f6531d365c4666ec8110d02dfea044
naeem <kazinaeemrayhan@gmail.com>
2022-04-05 00:37:23 +0600

    Blogs added

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -2,8 +2,33 @@ import React from "react";
 
 const Blogs = () => {
   return (
-    <div>
-      <h1>blogs</h1>
+    <div className="container my-5">
+      <h4 style={{ fontSize: "25px" }}>1. What is context api?</h4>
+      <p style={{ fontSize: "20px", marginLeft: "27px", marginBottom: "10px" }}>
+        React Context API যেকোনো component এর তথ্য অ্যাক্সেসের অনুমতি দেয়। এটি
+        grandparent থেকে parent এবং parent থেকে child এর কাছে data প্রদান করতে
+        পারে। এটি "prop drilling" এর বিকল্প হিসেবে ব্যবহার করা হয়। Context API
+        এর মাধমে grandparent থেকে child এ সরাসরি ডাটা পাঠানো যায়। Context API এর
+        মাধ্যমে Array, object, function ইত্যাদিকে পাঠানো যায়।
+      </p>
+      <h4 style={{ fontSize: "25px" }}>
+        2. Difference between inline and inline-block?
+      </h4>
+      <p style={{ fontSize: "20px", marginLeft: "27px", marginBottom: "5px" }}>
+        <span style={{ color: "darkorange", fontWeight: 600 }}>Inline: </span>
+        Inline element এটি একই লাইনে প্রদর্শন করে। Inline element নতুন লাইনে
+        শুরু হয় না এবং শুধুমাত্র এর element এর প্রস্থ নেয়। যদি কোনো Inline
+        element এ প্রস্থ এবং উচ্চতা সেট করার চেষ্টা করা হয়, তাহলে এর কোনো
+        প্রভাব থাকবে না।
+      </p>
+      <p style={{ fontSize: "20px", marginLeft: "27px", marginBottom: "10px" }}>
+        <span style={{ color: "darkorange", fontWeight: 600 }}>
+          Inline-block:{" "}
+        </span>
+        Inline-block এটি মূলত inline এর মতো একই জিনিস। এখানে প্রস্থ, উচ্চতা এবং
+        padding ব্যবহার করা যায়। Inline-block ও inline এর মতো একই লাইনে প্রদর্শন
+        করে।
+      </p>
     </div>
   );
 };

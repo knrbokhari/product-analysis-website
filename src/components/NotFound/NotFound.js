04a0f02a581a8f247b5f6f9561fdbd3c05443acd
naeem <kazinaeemrayhan@gmail.com>
2022-04-05 00:56:13 +0600

    404 Not Found added

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,9 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import NotFoundImg from "../../images/404-Icon.png";
 
 const NotFound = () => {
   return (
-    <div>
-      <h2>404 page not found..</h2>
+    <div className="container">
+      <img
+        src={NotFoundImg}
+        alt=""
+        style={{ maxWidth: "300px" }}
+        className="mx-auto d-block mt-5 mb-3"
+      />
+      <h3 className="mb-2">Something went wrong!</h3>
+      <p className="text-center fs-4 mb-1">404 page not found..</p>
+      <Link
+        to="/home"
+        className="text-decoration-none mx-auto d-block fs-5"
+        style={{ width: "101px" }}
+      >
+        Back to home
+      </Link>
     </div>
   );
 };
